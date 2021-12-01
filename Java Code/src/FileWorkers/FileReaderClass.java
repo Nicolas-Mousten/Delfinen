8d1eac5c1e1a9f204505f34b007124139771e314
lassetimm <lassetimm@outloo.dk>
2021-12-01 21:42:49 +0100

    Lasses chairman og FileReader ændringer. Jeg har endret filereador til fileReaderClass fordi den der typo irriterede mig

---
+++ Java Code/src/FileWorkers/FileReaderClass.java
@@ -0,0 +1,107 @@
+package FileWorkers;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.PrintWriter;
+
+public class FileReaderClass {
+    //Made by Lasse
+    public static void addLineToCsvFile(String memberName, int memberAge, boolean hasPaid, String getEmail){
+        try {
+            FileWriter writer = new FileWriter("Resources/MembersList.csv", true);
+
+            writer.append(memberName + "," + memberAge + "," + hasPaid + "," + getEmail);
+            writer.append("\n");
+
+            writer.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    //Made by Lasse
+    public static boolean removeLine(String searchTermToRemoveLine, String filePath, int position){
+        //Når man kalder denne metode til er fjerne i linje i en csv fil, skal man give den positionen på tingen man vil søge efter det er 'position'
+        //man skal give filens 'filePath' og hvad man skal søge efter for at finde den korrekte linje at slette.
+        int positionOfTerm = position - 1;
+        String tempFile = "temp.txt";
+        File oldFile = new File(filePath);
+        File newFile = new File(tempFile);
+        String currentLine;
+        String data[];
+        boolean checkForData = false;
+
+        try{
+            FileWriter fw = new FileWriter(tempFile,true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+
+            FileReader fr = new FileReader("Resources/MembersList.csv");
+            BufferedReader br = new BufferedReader(fr);
+
+            while((currentLine = br.readLine()) != null){                                 //currentLine bliver sat lig med den næste linje så længe den eksistere. Hvis næste linje ikke eksistere stopper while loopet.
+                data = currentLine.split(",");                                      //Her bliver den første linje i filen lagt i en array som splitter dem op i ','.
+                if(!(data[positionOfTerm].equalsIgnoreCase(searchTermToRemoveLine))){     //Her tjekker den om det du gerne vil havde fjernet er lig med den data der er gemt i data
+                    pw.println(currentLine);                                              //Hvis de ikke er lig med hinanden så gemmer vi den i den nye fil
+                }else{
+                    checkForData = true;
+                }
+            }
+
+            if (checkForData){
+                System.out.println("File has been deleted");
+            }else {
+                System.out.println("File not found");
+            }
+
+            pw.flush();
+            pw.close();
+            fr.close();
+            br.close();
+            bw.close();
+            fw.close();
+
+            oldFile.delete();
+            File temp = new File("Resources/MembersList.csv");
+            newFile.renameTo(temp);
+
+        }catch(Exception e){
+            System.out.println("Error");
+        }
+        return checkForData;
+    }
+    //Made by Lasse
+    public static void searchData(String searchTerm){
+        boolean found = false;
+        String name = "";
+        String age = "";
+        String email = "";
+        try {
+            FileReader writer = new FileReader("Resources/MembersList.csv");
+            Scanner scanner = new Scanner((Readable) writer);
+            scanner.useDelimiter("[,\n]");               //This line is to make java know that files are separated by ',' and after each new line '\n'
+
+            while(scanner.hasNext() && !found )
+            {
+                name = scanner.next();
+                age = scanner.next();
+                email = scanner.next();
+
+                if(name.equals(searchTerm) || age.equals(searchTerm) || email.equals(searchTerm)){
+                    found = true;
+                }
+            }
+            if (found) {
+                System.out.println(name + " " + age + " " + email);
+            } else {
+                System.out.println("There was nothing");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
--- Java Code/src/membersClasses/Chairman.java
+++ Java Code/src/membersClasses/Chairman.java
@@ -1,6 +1,6 @@
 package membersClasses;
 import java.util.Scanner;
-import FileWorkers.FileReador;
+import FileWorkers.FileReaderClass;
 
 public class Chairman extends Cashier{
     public Chairman(String name, int age, String email, boolean hasPaid, boolean isPartOfStaff, boolean isPassive) {
@@ -12,9 +12,65 @@ public class Chairman extends Cashier{
     }
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         Chairman chairman = new Chairman("benny",10,true,true,true);
-        inputNewMembers(scanner);
+
+        chairmanMenu(scanner);
+
+
+    }
+    //Made by Lasse
+
+    public static void chairmanMenu(Scanner scanner){
+        boolean whileKey = true;
+        do{
+            printChairmanMenu();
+            System.out.println("Please write a number for the option you would like to choose");
+            int menuChoice = scanner.nextInt();
+            scanner.nextLine();
+            switch(menuChoice) {
+                case 1:
+                    removeMember(scanner);
+                    whileKey = false;
+                    break;
+                case 2:
+                    inputNewMembers(scanner);
+                    whileKey = false;
+                    break;
+                case 3:
+                    System.out.println("Please write what you want to search after");
+                    String searchTerm = scanner.nextLine();
+                    FileReaderClass.searchData(searchTerm);
+                    whileKey = false;
+                    break;
+                case 4:
+                    whileKey = false;
+                    break;
+
+            }
+
+        }while(whileKey);
+    }
+
+    public static void printChairmanMenu() {
+        String[] ChairmanMenu = {"Remove member", "Add member", "Search data", "Exit program"};
+        int[] ChairmanMenuNumbers = {1, 2, 3, 4};
+        for (int i = 0; i < ChairmanMenuNumbers.length; i++) {
+            System.out.println(ChairmanMenuNumbers[i] + " " + ChairmanMenu[i]);
+        }
+    }
+
+    public static void removeMember(Scanner scanner){
+        String filePath = "Resources/MembersList.csv";
+        int position = 4;                                   //Dette er positionen i csv filen jeg vil søge efter. Vi går efter email da det er unik for hver member
+                                                            //og email ligger på position 4
+        boolean checkForData = false;
+        do {
+            System.out.println("Please write the email of the member you would like to delete");
+            String removeUser = scanner.nextLine();
+            checkForData = FileReaderClass.removeLine(removeUser, filePath, position);
+        }while(!checkForData);
     }
 
     public static void inputNewMembers(Scanner scanner){
@@ -26,15 +82,15 @@ public class Chairman extends Cashier{
         boolean hasPaid = hasMemberPaid(scanner);  //Hvis den er false har de ikke betalt, hvis den er true har de betalt
         String getEmail = getMemberEmail(scanner);
 
-        FileReador.addMemberToCsvFile(memberName, memberAge, hasPaid, getEmail);
+        FileReaderClass.addLineToCsvFile(memberName, memberAge, hasPaid, getEmail);
     }
-
+    //Made by Lasse
     public static String getMemberEmail(Scanner scanner){
         System.out.println("Please type in member e-mail");
         String email = scanner.nextLine();
         return email;
     }
-
+    //Made by Lasse
     public static boolean hasMemberPaid(Scanner scanner) {
         boolean hasPaid = false;
         String memberAnswer = "";
@@ -49,7 +105,7 @@ public class Chairman extends Cashier{
         }while(!memberAnswer.equalsIgnoreCase("y") && !memberAnswer.equalsIgnoreCase("n"));
         return hasPaid;
     }
-
+    //Made by Lasse
     public static boolean checkMemberPay(String memberAnswer, boolean hasPaid){
         if (memberAnswer.equals("y")) {
             hasPaid = true;
