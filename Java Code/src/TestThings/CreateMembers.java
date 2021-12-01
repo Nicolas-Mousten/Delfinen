bb140675ba6fb9df4a6915540df253ab672b97c4
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-12-01 12:20:00 +0100

    Nicolas har lige fixed keywords for fileReader

--- Java Code/src/TestThings/CreateMembers.java
+++ Java Code/src/TestThings/CreateMembers.java
@@ -19,19 +19,6 @@ public class CreateMembers {
         Member nicolas = new Member("Nicolas", 21, "nicolas.mousten@gmail.com", true);
         Member lasse = new Member("Lasse", 23, "LasseTimm@outlook.com", false, false, true);
         Member tobias = new Member("Tobias", 24, "TobiasDenBedste@Gmail.com", true);
-        try {
-            // Here BufferedWriter is used to write the objects to MembersList
-            // Since buffered writer inherits from writer it will also
-            // Append means it will write to the end of the document instead of the beginning
-            FileWriter fileWriter = new FileWriter(membersList, true);
-            BufferedWriter buffer = new BufferedWriter(fileWriter);
-            buffer.write(nicolas.toString());
-            buffer.newLine();
-            buffer.close();
-            System.out.println("new member added to MembersList.csv");
-        } catch (IOException e) {
-            System.out.println("There was an error");
-            e.getStackTrace();
-        }
+        
     }
 }
