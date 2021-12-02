822648f67c9001469a1130730470521ae6ef540f
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-12-02 09:54:12 +0100

    Nicolas, lavet kunde base

--- Java Code/src/System.java
+++ Java Code/src/System.java
@@ -1,6 +1,16 @@
 import UI.LoginUI;
+import membersClasses.Chairman;
 public class System {
     public static void main(String[] args) {
-        LoginUI.startMenu();
+        int loginUser = LoginUI.startMenu();
+        switch (loginUser) {
+            case 1:
+
+            case 2:
+
+            case 3:
+                Chairman.chairmanMenu();
+
+        }
     }
 }
