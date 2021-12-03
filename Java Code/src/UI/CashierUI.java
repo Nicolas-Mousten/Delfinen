641c062e4dbe2d905300f24b0e16acc410f73225
Toby-byte <tobiasbogenielsen@gmail.com>
2021-12-03 10:49:07 +0100

    "toby-byte" made CashierUI.java loop and added quit function

--- Java Code/src/UI/CashierUI.java
+++ Java Code/src/UI/CashierUI.java
@@ -1,5 +1,6 @@
 package UI;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 import static membersClasses.Cashier.promptChairmanToRemoveMember;
@@ -7,23 +8,26 @@ import static membersClasses.Cashier.viewContigent;
 
 public class CashierUI {
     public static void cashierMenu() {
+
+        int menuChoice = 0;
+
+        do {
         Scanner scanner = new Scanner(System.in);
-        Boolean key = true;
         System.out.println("Please choose an option");
         System.out.println("1.      View Contingent");
         System.out.println("2.      Prompt Chairman to remove a member");
-        int menuChoice = scanner.nextInt();
+        System.out.println("3.      press \"q\" to quit");
+        menuChoice = scanner.nextInt();
         scanner.nextLine();
 
-        do {
             switch (menuChoice) {
                 case 1: viewContigent();
-                    key = false;
                     break;
                 case 2: promptChairmanToRemoveMember();
-                    key = false;
                     break;
+                case 3:
+                    System.out.println("Goodbye");
             }
-        } while (key);
+        } while (menuChoice != 3);
     }
 }
