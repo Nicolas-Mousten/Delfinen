cdc5b6aef81b81f262112df8371445fb087b7cff
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-12-07 12:56:16 +0100

    Nicolas - added try catch failsafes

--- Java Code/src/UI/ChooseDisciplineUI.java
+++ Java Code/src/UI/ChooseDisciplineUI.java
@@ -1,10 +1,10 @@
 package UI;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 // Importing FileReader and TrainingGroup classes
 import FileWorkers.FileReaderClass;
-import TrainingGroup.TrainingGroup;
 import TrainingGroup.BreastSwimming;
 import TrainingGroup.Butterfly;
 import TrainingGroup.BackCrawl;
@@ -18,68 +18,78 @@ public class ChooseDisciplineUI
     static Scanner scanner = new Scanner(System.in);
 
     // Choose swimming discipline
-    public void chooseSwimmingDiscipline()
+    public static void chooseSwimmingDiscipline()
     {
+        boolean whileKey;
+        do {
+            try{
+            whileKey = true;
+            // Enter number of swimmers
+            System.out.println("Enter number of swimmers");
+            int numberOfSwimmers = scanner.nextInt();
 
-        // Enter number of swimmers
-        System.out.println("Enter number of swimmers");
-        int numberOfSwimmers = scanner.nextInt();
-
-        // Choose discipline
-        System.out.println("Choose swimming discipline. Press 1 for crawl. Press 2 for back crawl. Press 3 for breast swimming. Press 4 for butterfly times");
-
-
-        // Number to choose discipline
-        int chooseSwimmingDiscipline = scanner.nextInt();
-
-        // Setting member name, member e-mail and member swimming time equal to their corresponding methods
-        String memberName = returnMemberName();
-        String returnEmail = returnMemberEmail();
-        double returnSwimmerTime = returnMemberTime();
-
-        // Setting the file path per default to an empty string (it's value is being set to the swimming discipline's corresponding CSV file)
-        String filePath = "";
-
-        // Discipline choices
-        switch (chooseSwimmingDiscipline)
-        {
+            // Choose discipline
+            System.out.println("Choose swimming discipline. Press 1 for crawl. Press 2 for back crawl. Press 3 for breast swimming. Press 4 for butterfly times");
 
 
-            // 1. Crawl
-            case 1:
-                filePath = "Resources/crawltimes.csv";
-                System.out.println("Enter crawl times");
-                Crawl.registerCrawlTimes(numberOfSwimmers);
-                FileReaderClass.addSwimmerToCsvFile(memberName, returnEmail, returnSwimmerTime, filePath);
-                break;
+            // Number to choose discipline
+            int chooseSwimmingDiscipline = scanner.nextInt();
+
+            // Setting member name, member e-mail and member swimming time equal to their corresponding methods
+            String memberName = returnMemberName();
+            String returnEmail = returnMemberEmail();
+            double returnSwimmerTime = returnMemberTime();
+
+            // Setting the file path per default to an empty string (it's value is being set to the swimming discipline's corresponding CSV file)
+            String filePath = "";
+
+            // Discipline choices
+            switch (chooseSwimmingDiscipline) {
+                // 1. Crawl
+                case 1:
+                    filePath = "Resources/crawltimes.csv";
+                    System.out.println("Enter crawl times");
+                    Crawl.registerCrawlTimes(numberOfSwimmers);
+                    FileReaderClass.addSwimmerToCsvFile(memberName, returnEmail, returnSwimmerTime, filePath);
+                    whileKey = false;
+                    break;
 
 
-            // 2. Back Crawl
-            case 2:
-                filePath = "Resources/backcrawltimes.csv";
-                System.out.println("Enter back crawl times");
+                // 2. Back Crawl
+                case 2:
+                    filePath = "Resources/backcrawltimes.csv";
+                    System.out.println("Enter back crawl times");
 
 
-                BackCrawl.registerBackCrawlTimes(numberOfSwimmers);
-                FileReaderClass.addSwimmerToCsvFile(memberName, returnEmail, returnSwimmerTime, filePath);
-                break;
+                    BackCrawl.registerBackCrawlTimes(numberOfSwimmers);
+                    FileReaderClass.addSwimmerToCsvFile(memberName, returnEmail, returnSwimmerTime, filePath);
+                    whileKey = false;
+                    break;
 
 
-            // 3. Breast Swimming
-            case 3:
-                filePath = "Resources/breastswimmingtimes.csv";
-                System.out.println("Enter breast swimming times");
-                BreastSwimming.registerBreastSwimmingTimes(numberOfSwimmers);
-                break;
+                // 3. Breast Swimming
+                case 3:
+                    filePath = "Resources/breastswimmingtimes.csv";
+                    System.out.println("Enter breast swimming times");
+                    BreastSwimming.registerBreastSwimmingTimes(numberOfSwimmers);
+                    whileKey = false;
+                    break;
 
 
-            // 4. Butterlfy
-            case 4:
-                filePath = "Resources/butterflytimes.csv";
-                System.out.println("Enter butterfly times");
-                Butterfly.registerButterflyTimes(numberOfSwimmers);
-                break;
-        }
+                // 4. Butterlfy
+                case 4:
+                    filePath = "Resources/butterflytimes.csv";
+                    System.out.println("Enter butterfly times");
+                    Butterfly.registerButterflyTimes(numberOfSwimmers);
+                    whileKey = false;
+                    break;
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Invalid Input");
+                whileKey = true;
+                scanner.nextLine();
+            }
+        }while(whileKey);
     }
 
 
@@ -100,7 +110,7 @@ public class ChooseDisciplineUI
     }
 
     // Return swimmer time
-    public double returnMemberTime()
+    public static double returnMemberTime()
     {
         System.out.println("Please enter member's time");
         double time = scanner.nextDouble();
--- Java Code/src/UI/LoginUI.java
+++ Java Code/src/UI/LoginUI.java
@@ -1,6 +1,7 @@
 // Written by Nicolas
 package UI;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LoginUI
@@ -18,45 +19,47 @@ public class LoginUI
         //
         int choice = 0;
         int returnValue = 0;
-
+        boolean whileKey;
         // Do everything in the do block
-        do
-        {
-            System.out.print("Choose 1 to log in as trainer\nChoose 2 to log in as cashier\nChoose 3 to log in as Chairman\n>");
+        do {
+            try {
+                System.out.print("Choose 1 to log in as trainer\nChoose 2 to log in as cashier\nChoose 3 to log in as Chairman\n>");
+                whileKey =false;
+                // Getting choice
+                choice = sc.nextInt();
+                sc.nextLine();
 
-            // Getting choice
-            choice = sc.nextInt();
-            sc.nextLine();
-
-            // If choice is 1
-            if (choice == 1)
-            {
-                // Return 1 and print message
-                returnValue = 1;
-                System.out.println("Welcome trainer");
-            }
-            // If choice is 2
-            else if (choice == 2)
-            {
-                // Return 2 and print message
-                returnValue = 2;
-                System.out.println("Welcome Cashier");
-            }
-            // If choice is 3
-            else if (choice == 3)
-            {
-                // Return 3 and print message
-                returnValue = 3;
-                System.out.println("Welcome Chairman");
-            }
-            // Else print out error message
-            else
-            {
+                // If choice is 1
+                if (choice == 1) {
+                    // Return 1 and print message
+                    returnValue = 1;
+                    System.out.println("Welcome trainer");
+                }
+                // If choice is 2
+                else if (choice == 2) {
+                    // Return 2 and print message
+                    returnValue = 2;
+                    System.out.println("Welcome Cashier");
+                }
+                // If choice is 3
+                else if (choice == 3) {
+                    // Return 3 and print message
+                    returnValue = 3;
+                    System.out.println("Welcome Chairman");
+                }
+                // Else print out error message
+                else {
+                    System.out.println("Not a legit answer");
+                    whileKey = true;
+                }
+            }catch(InputMismatchException e){
                 System.out.println("Not a legit answer");
+                whileKey = true;
+                sc.nextLine();
             }
         }
         // While choice is between 1 and 3 (the two boundaries included)
-        while (choice < 1 || choice > 3);
+        while (whileKey);
 
         // Return the return value
         return returnValue;
